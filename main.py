73935501ade331b592eb63cc215dcfeade63d487
wombi <hackermans@myyahoo.com>
2025-12-03 09:17:18 -0600

    adds word counter method

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+def main():
+    book = get_book_text("books/frankenstein.txt")
+    total_num_of_words = word_count(book)
+    print(f"Found {total_num_of_words} total words")
+
+    
+def word_count(book_as_a_string):
+    split_string = book_as_a_string.split()
+    num_of_words = len(split_string)
+    return num_of_words
+
+main()
