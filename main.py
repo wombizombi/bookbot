d35438ea118c67b325e9e6cbc77236fad2c82395
wombi <hackermans@myyahoo.com>
2025-12-06 15:22:33 -0600

    adds imported methods from stats.py to count characters

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
+from stats import word_count
+from stats import char_count
+from stats import sorted_list_of_dictionaries
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
-def main():
-    book = get_book_text("books/frankenstein.txt")
-    total_num_of_words = word_count(book)
-    print(f"Found {total_num_of_words} total words")
+def sort_on(items):
+    return items["num"]
 
-    
-def word_count(book_as_a_string):
-    split_string = book_as_a_string.split()
-    num_of_words = len(split_string)
-    return num_of_words
+def main():
+
+    path = "books/frankenstein.txt"
+    book = get_book_text(path)
+    total_num_of_words = word_count(book)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_num_of_words} total words")
+    char_counted = char_count(book)
+    sorted_list = sorted_list_of_dictionaries(char_counted)
+    sorted_list.sort(reverse=True, key=sort_on)
+    print("--------- Character Count -------")
+    for item in sorted_list:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+
 
 main()
