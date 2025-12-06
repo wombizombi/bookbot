a0dd3e1f8864a2b4c29a1b1a869de5e03205c157
wombi <hackermans@myyahoo.com>
2025-12-06 17:18:53 -0600

    imports sys to pass text as arguements from the command line to the char counter program

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 from stats import word_count
 from stats import char_count
 from stats import sorted_list_of_dictionaries
+
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -11,9 +13,14 @@ def sort_on(items):
     return items["num"]
 
 def main():
-
-    path = "books/frankenstein.txt"
+    path = ""
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
     book = get_book_text(path)
+    
     total_num_of_words = word_count(book)
 
     print("============ BOOKBOT ============")
